67b67cd99b7c48bca564760a85a762b4b2739b4d
sidhant-sriv <sidhant.sriv@gmail.com>
2023-12-07 17:25:47 +0530

    feat: removed django and added text routes

---
+++ file-server/moviegen.py
@@ -0,0 +1,56 @@
+import os
+from PIL import Image
+from moviepy.editor import AudioFileClip, ImageSequenceClip, concatenate_videoclips
+
+def resize_images(folder_path):
+    """
+    Resize all images in the given folder to a 16:9 portrait ratio and overwrite them.
+
+    Parameters:
+    - folder_path (str): The path to the folder containing the images.
+    """
+    # Ensure the folder path is valid
+    if not os.path.isdir(folder_path):
+        print(f"Error: {folder_path} is not a valid directory.")
+        return
+
+    # Get a list of all files in the folder
+    files = os.listdir(folder_path)
+
+    # Filter only image files
+    image_files = [file for file in files if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
+
+    # Resize and overwrite each image
+    for image_file in image_files:
+        image_path = os.path.join(folder_path, image_file)
+
+        # Open the image
+        with Image.open(image_path) as img:
+            # Calculate the new width based on a 16:9 ratio
+            new_width = int(img.height * (9 / 16))
+
+            # Resize the image to the new dimensions
+            resized_img = img.resize((new_width, img.height), Image.ANTIALIAS)
+
+            # Overwrite the original image
+            resized_img.save(image_path)
+
+    return folder_path
+
+
+def create_video(audio_file, image_folder='file-server/static/images', output_file="file-server/static/movie/output_video.mp4"):
+    # img_folder = resize_images(image_folder)
+    image_files = [os.path.join(image_folder, img) for img in sorted(os.listdir(image_folder))]
+    audio_clip = AudioFileClip(audio_file)
+    audio_duration = audio_clip.duration
+    num_images = len(image_files)
+    duration_per_image = audio_duration / num_images
+    image_clips = [ImageSequenceClip([image_file], fps=1 / duration_per_image) for image_file in image_files]
+    video_clip = concatenate_videoclips(image_clips, method="compose")
+    video_clip = video_clip.set_duration(audio_duration)
+    video_clip = video_clip.resize(width=900, height=1600)
+    video_clip = video_clip.set_audio(audio_clip)
+    output_video_file = output_file.replace('.mp4', '_with_audio.mp4')
+    video_clip.write_videofile(output_video_file, codec='libx264', audio_codec='aac', threads=4, preset='ultrafast')
+    return output_video_file
+
--- server/server/taketext/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TaketextConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "taketext"
